5925eef96a9f38523781827ebf9e862a266e2830
ainokuos <aino.kuosmanen@helsinki.fi>
2022-05-31 10:30:37 +0300

    tests

--- app/imports/tests.py
+++ app/imports/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+import tempfile, csv, os
 
-# Create your tests here.
+class ImportViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='Testi')
+        self.client.force_login(self.user)
+        #self.user = User.objects.create_user('testi', 'testi@jotain.com', '1234')
+        #self.client.login(username='testi', password='1234')
+    
+    def test_import_test_view(self):
+        response = self.client.get('/import/test')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_import_test_reverse(self):
+        response = self.client.get(reverse('import_test'))
+        self.assertEqual(response.status_code, 200)
