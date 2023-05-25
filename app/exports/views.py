5e7c2c10adc4205b2dbf12517406fddac203353c
Antti Hartikka <antti.hartikka@helsinki.fi>
2023-05-25 14:58:29 +0300

    Celery environment set up.
    Co-authored-by: Anne Havunen <anne.havunen@helsinki.fi>

--- app/exports/views.py
+++ app/exports/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . import tasks
 
 
 def export_hello_world(request):
+    tasks.hello_world_celery.apply_async(countdown=10)
     return render(request, "export/export_hello.html")
