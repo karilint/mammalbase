c04f99ae4b07ca524e7bf4ecaec18159103dbb40
Kaisa Korhonen <kaisa@xd.fi>
2023-05-26 08:53:47 +0300

    New feature: tsv export with dummy data from database

--- app/exports/views.py
+++ app/exports/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import StreamingHttpResponse
+from mb.models import ViewMasterTraitValue
 
 import csv
 
@@ -12,14 +13,15 @@ class Echo:
         """Write the value by returning it, instead of storing in a buffer."""
         return value
 
-
 def export_to_tsv(request):
-    """A view that streams a large CSV file."""
-    rows = (["Row {}".format(idx), str(idx)] for idx in range(10))
+    """A view that streams a large TSV file."""
+    measurements = ViewMasterTraitValue.objects.all()
     pseudo_buffer = Echo()
-    writer = csv.writer(pseudo_buffer)
+    writer = csv.writer(pseudo_buffer, delimiter='\t', lineterminator='\n')
+    writer.writerow(['id', 'master_id', 'master_entity_name', 'master_attribute_id', 'master_attribute_name', 'traits_references', 'assigned_values', 'n_distinct_value', 'n_value', 'n_supporting_value', 'trait_values', 'trait_selected', 'trait_references', 'value_percentage'])
+    msr = measurements.values_list('id', 'master_id', 'master_entity_name', "master_attribute_id", 'master_attribute_name', 'traits_references', 'assigned_values', 'n_distinct_value', 'n_value', 'n_supporting_value', 'trait_values', 'trait_selected', 'trait_references', 'value_percentage')
     return StreamingHttpResponse(
-        (writer.writerow(row) for row in rows),
-        content_type="text/csv",
-        headers={"Content-Disposition": 'attachment; filename="mammalbase.csv"'},
+        (writer.writerow(m) for m in msr),
+        content_type="text/tsv",
+        headers={"Content-Disposition": 'attachment; filename="measurements.tsv"'},
     )  
