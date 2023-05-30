7c789e0b65b0cc6da6797281b52ca444dee12272
Anne Havunen <anne.havunen@helsinki.fi>
2023-05-30 08:59:04 +0300

    Create function: export zip file
    Co-authored-by: Antti Hartikka <antti.hartikka@helsinki.fi>

--- app/exports/tasks.py
+++ app/exports/tasks.py
@@ -7,6 +7,8 @@ import csv
 from django.core.files import File
 from .models import ExportFile
 from django.core.files.base import ContentFile
+import io
+import zipfile
 
 
 @shared_task
@@ -17,20 +19,32 @@ def hello_world_celery():
 @shared_task
 def create_poc_tsv_file():
     measurements = ViewMasterTraitValue.objects.all()
-    msr = measurements.values_list('id', 'master_id', 'master_entity_name', "master_attribute_id",
+    msr = measurements.values_list('id', 'master_id', 'master_entity_name', 'master_attribute_id',
                                    'master_attribute_name', 'traits_references', 'assigned_values', 'n_distinct_value',
                                    'n_value', 'n_supporting_value', 'trait_values', 'trait_selected',
                                    'trait_references', 'value_percentage')
-    filename = f'export_measurements_{random.randint(0,32000)}.tsv'
-    with open(filename, 'w+') as f:
-        django_file = File(f)
-        writer = csv.writer(django_file, delimiter='\t', lineterminator='\n')
-        writer.writerow(
-            ['id', 'master_id', 'master_entity_name', 'master_attribute_id', 'master_attribute_name', 'traits_references',
-            'assigned_values', 'n_distinct_value', 'n_value', 'n_supporting_value', 'trait_values', 'trait_selected',
-            'trait_references', 'value_percentage'])
-        for m in msr:
-            writer.writerow(m)
-        file_model = ExportFile(name=filename, file=django_file)
+    filename = f'export_measurements_{random.randint(0,32000)}.tsv' # oli .zip
+    f = open(filename, 'w+', errors='replace')
+    writer = csv.writer(f, delimiter='\t', lineterminator='\n')
+    writer.writerow(
+        ['id', 'master_id', 'master_entity_name', 'master_attribute_id', 'master_attribute_name', 'traits_references',
+        'assigned_values', 'n_distinct_value', 'n_value', 'n_supporting_value', 'trait_values', 'trait_selected',
+        'trait_references', 'value_percentage'])
+    for m in msr:
+        writer.writerow(m)
+    f.close()
+
+    with open(zip_files( [ filename ] ), 'rb') as zip:
+        django_file = File(zip)
+        print(f"DJANGO FILE :::: {django_file}")
+        file_model = ExportFile(name="export_measurements.zip", file=django_file)
         file_model.save()
         print(f'Created new file. ID = {file_model.pk}')
+
+def zip_files(files):
+    temp_zip = zipfile.ZipFile('tempzip.zip', 'w', compression=zipfile.ZIP_DEFLATED)
+    for file in files:
+        temp_zip.write(file)
+    temp_zip.close()
+    return 'tempzip.zip'
+
