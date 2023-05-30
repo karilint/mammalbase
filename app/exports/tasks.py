018163131f3dbfb5ee4fa04e0598668069868b83
Antti Hartikka <antti.hartikka@helsinki.fi>
2023-05-31 00:04:55 +0300

    divided zip export to more sensible parts

--- app/exports/tasks.py
+++ app/exports/tasks.py
@@ -7,39 +7,67 @@ from django.core.files import File
 from .models import ExportFile
 from datetime import datetime
 from django.db.models.query import QuerySet
+from zipfile import ZipFile
 
 
 @shared_task
-def export_zip_file(queries: [QuerySet]):
-    files = []
+def export_zip_file(queries: [(str, [str], QuerySet)]):
     temp_directory = f'temp_{datetime.now()}'
     os.mkdir(temp_directory)
-    for i, query in enumerate(queries):
-        file_path = f'{temp_directory}/export_{i}.tsv'
-        with open(file_path, 'w') as f:
-            files.append(file_path)
-            writer = csv.writer(f, delimiter='\t', lineterminator='\n')
-            writer.writerows(query().values_list())
-    temp_zip_file_path = zip_files(files)
-    with open(temp_zip_file_path, 'rb') as zip_file:
+    os.chdir(temp_directory)
+
+    zip_file_path = f'export_{datetime.now()}.zip'
+    temp_zip_writer = ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED)
+
+    for file_name, headers, query in queries:
+        file_path = write_query_to_file(file_name, headers, query)
+        temp_zip_writer.write(file_path)
+
+    temp_zip_writer.close()
+
+    file_id = save_zip_to_django_model(zip_file_path)
+    print(f'Created new file: http://localhost:8000/exports/get_file/{file_id}')
+
+    os.chdir('..')
+    shutil.rmtree(temp_directory)
+
+
+def write_query_to_file(file_name, headers, query):
+    file_path = f'{file_name}.tsv'
+    f = open(file_path, 'w')
+    writer = csv.writer(f, delimiter='\t', lineterminator='\n')
+    writer.writerow(headers)
+    writer.writerows(query.values_list())
+    f.close()
+    return file_path
+
+
+def save_zip_to_django_model(zip_file_path):
+    with open(zip_file_path, 'rb') as zip_file:
         django_file = File(zip_file)
         file_model = ExportFile(file=django_file)
         file_model.save()
-        print(f'Created new file: http://localhost:8000/exports/get_file/{file_model.pk}')
-    os.remove(temp_zip_file_path)
-    shutil.rmtree(temp_directory)
+        return file_model.pk
+
 
 
 @shared_task
 def create_poc_tsv_file():
-    export_zip_file([ViewMasterTraitValue.objects.all , ExportFile.objects.all])
+    export_zip_file([
+        ('ViewMasterTraitValue.objects.all',
+            ['h1', 'h2', 'h3', 'etc...'],
+            ViewMasterTraitValue.objects.all()),
+        ('ExportFile.objects.all',
+            ['h1', 'h2', 'h3', 'etc...'],
+            ExportFile.objects.all())
+    ])
 
 
 def zip_files(files):
-    file_name = f'export_{datetime.now()}.zip'
-    temp_zip = zipfile.ZipFile(file_name, 'w', compression=zipfile.ZIP_DEFLATED)
+    zip_file_name = f'export_{datetime.now()}.zip'
+    temp_zip = ZipFile(zip_file_name, 'w', compression=zipfile.ZIP_DEFLATED)
     for file in files:
         temp_zip.write(file)
     temp_zip.close()
-    return file_name
+    return zip_file_name
 
