a41e2549de51290464a187ad78b0f6897c57b10e
Antti Hartikka <antti.hartikka@helsinki.fi>
2023-05-31 10:05:00 +0300

    refactored export function to use kwargs instead of tuple

--- app/exports/tasks.py
+++ app/exports/tasks.py
@@ -7,41 +7,43 @@ from django.core.files import File
 from django.core.mail import send_mail
 from .models import ExportFile
 from datetime import datetime
-from django.db.models.query import QuerySet
 from zipfile import ZipFile
 from config import settings
 from tempfile import mkdtemp
 
 
 @shared_task
-def export_zip_file(queries: [(str, [str], QuerySet)]):
+def export_zip_file(kwargs):
     current_dir = os.getcwd()
     temp_directory = mkdtemp()
     os.chdir(temp_directory)
 
-    zip_file_path = f'export_{datetime.now()}.zip'
+    zip_file_path = f'export_{datetime.now().strftime("%Y-%m-%d_%H.%M.%S.%f")}.zip'
     temp_zip_writer = ZipFile(zip_file_path, 'w', compression=zipfile.ZIP_DEFLATED)
 
-    for file_name, headers, query in queries:
-        file_path = write_query_to_file(file_name, headers, query)
+    for query in kwargs['queries']:
+        file_path = write_query_to_file(query)
         temp_zip_writer.write(file_path)
 
     temp_zip_writer.close()
 
     file_id = save_zip_to_django_model(zip_file_path)
 
-    send_email(file_id, 'testi@testaaja.com')
+    send_email(file_id, kwargs['email_receiver'])
 
     os.chdir(current_dir)
     shutil.rmtree(temp_directory)
 
 
-def write_query_to_file(file_name, headers, query):
+def write_query_to_file(query):
+    file_name = query['file_name']
+    headers = query['headers']
+    query_set = query['query_set']
     file_path = f'{file_name}.tsv'
     f = open(file_path, 'w')
     writer = csv.writer(f, delimiter='\t', lineterminator='\n')
     writer.writerow(headers)
-    writer.writerows(query.values_list())
+    writer.writerows(query_set.values_list())
     f.close()
     return file_path
 
@@ -57,23 +59,16 @@ def save_zip_to_django_model(zip_file_path):
 
 @shared_task
 def create_poc_tsv_file():
-    export_zip_file([
-        ('ViewMasterTraitValue.objects.all',
-            ['h1', 'h2', 'h3', 'etc...'],
-            ViewMasterTraitValue.objects.all()),
-        ('ExportFile.objects.all',
-            ['h1', 'h2', 'h3', 'etc...'],
-            ExportFile.objects.all())
-    ])
+    export_zip_file({
+        'email_receiver': 'testi@testipaikka.com',
+        'queries': [{'file_name': 'ViewMasterTraitValue.objects.all',
+                     'headers': ['h1', 'h2', 'h3', 'etc...'],
+                     'query_set': ViewMasterTraitValue.objects.all()},
+                    {'file_name': 'ExportFile.objects.all',
+                     'headers': ['h1', 'h2', 'h3', 'etc...'],
+                     'query_set': ExportFile.objects.all()}
+                    ]})
 
-
-def zip_files(files):
-    zip_file_name = f'export_{datetime.now()}.zip'
-    temp_zip = ZipFile(zip_file_name, 'w', compression=zipfile.ZIP_DEFLATED)
-    for file in files:
-        temp_zip.write(file)
-    temp_zip.close()
-    return zip_file_name
 
 @shared_task
 def send_email(export_id, target_address):
@@ -90,5 +85,5 @@ def send_email(export_id, target_address):
 def create_message(export_id):
     '''
     Creates email message and download link 
-    '''    
+    '''
     return f'You can download your exported data from http://localhost:8000/exports/get_file/{export_id}'
