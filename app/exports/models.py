3f0270bff9c6de435808804c3deb5342573c47c3
Antti Hartikka <antti.hartikka@helsinki.fi>
2023-06-03 15:07:13 +0300

    cleanup

--- app/exports/models.py
+++ app/exports/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.db.models import Model, ForeignKey
 from django.core.files.storage import default_storage
 
 
-class ExportFile(Model):
+class ExportFile(models.Model):
     file = models.FileField(default_storage)
 
     def __str__(self):
