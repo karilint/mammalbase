28bb178b1c1df59ffd69478020a505d0eee05321
Antti Hartikka <antti.hartikka@helsinki.fi>
2023-06-07 12:09:04 +0300

    created new date_format function
    Co-authored-by: Anne Havunen <anne.havunen@helsinki.fi>

--- app/exports/query_sets/custom_db_functions.py
+++ app/exports/query_sets/custom_db_functions.py
@@ -1,6 +1,11 @@
 from django.db.models import Func
 
 
+class DateFormat(Func):
+    function = 'DATE_FORMAT'
+    template = '%(function)s(%(expressions)s)'
+
+
 class Round2(Func):
     function = 'ROUND'
     template = '%(function)s(%(expressions)s, 2)'
