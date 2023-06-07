99ccf50caeb74425496afa60b4e410ee3c279b3f
Antti Hartikka <antti.hartikka@helsinki.fi>
2023-06-07 13:54:51 +0300

    metadata query ver 1
    Co-authored-by: Anne Havunen <anne.havunen@helsinki.fi>

---
+++ app/exports/query_sets/measurements/metadata.py
@@ -0,0 +1,59 @@
+from exports.query_sets.measurements.base_query import query as base_query
+from django.db.models.functions import Now, Concat, TruncYear
+from django.db.models import Value, Subquery, OuterRef, CharField
+from allauth.socialaccount.models import SocialAccount
+from exports.query_sets.custom_db_functions import DateFormat
+import datetime
+from datetime import timezone, datetime, timedelta
+
+now = str(datetime.now(tz=timezone(timedelta(hours=2))))
+now1 = str(datetime.now(tz=timezone(timedelta(hours=2))).strftime('%Y-%m-%dT%H:%M+02:00'))
+now2 = str(datetime.now(tz=timezone(timedelta(hours=2))).strftime('%d %m %Y'))
+year = str(datetime.now(tz=timezone(timedelta(hours=2))).year)
+
+query = base_query.annotate(
+    dataset_id=Value('https://urn.fi/urn:nbn:fi:att:8dce459f-1401-4c6a-b2bb-c831bd8d3d6f'),
+    dataset_name=Value('MammalBase — Dataset 03: Trait Data in Ecological Trait-data Standard (ETS) format'),
+    dataset_description=Value(
+        'MammalBase - www.mammalbase.net: Trait dataset output in Ecological Trait-data Standard (ETS)'
+    ),
+    orcid_uid=Subquery(
+        SocialAccount.objects.filter(
+            user_id=OuterRef('created_by__id')
+        ).values_list('uid')[:1]
+    ),
+    issued=Value(now),
+    version=Value(now1),
+    bibliographic_citation=Concat(
+        Value('The MammalBase community '),
+        Value(now),
+        Value(' , Data version '),
+        Value(now2),
+        Value(' at https://mammalbase.org/me/'),
+        output_field=CharField()
+    ),
+    conforms_to=Value(
+        ('Ecological Trait-data Standard Vocabulary; v0.10; '
+        'URL: https://terminologies.gfbio.org/terms/ets/pages/; '
+        'URL: https://doi.org/10.5281/zenodo.1485739')
+    ),
+    rights_holder=Value(
+        'Lintulaakso, Kari;https://orcid.org/0000-0001-9627-8821;Finnish Museum of Natural History LUOMUS'
+    ),
+    rights=Value('Attribution 4.0 International (CC BY 4.0)'),
+    licence=Value('CC BY 4.0')
+).distinct()
+
+fields = [
+    ('dataset_id', 'datasetID'),
+    ('dataset_name', 'datasetName'),
+    ('dataset_description', 'datasetDescription'),
+    ('orcid_uid', 'author'),
+    ('issued', 'issued'),
+    ('version', 'version'),
+    ('bibliographic_citation', 'bibliographicCitation'),
+    ('conforms_to', 'conformsTo'),
+    ('rights_holder', 'rightsHolder'),
+    ('rights', 'rights'),
+    ('licence', 'license'),
+]
