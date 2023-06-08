0aecd4e0944c7a5265e897bc391d08c681089a52
Essi Prykäri <essi.prykari@helsinki.fi>
2023-06-08 13:56:53 +0300

    Changed export measurements to export ETS and updated export form
    Co-authored-by: kemiliak <kemiliak@users.noreply.github.com>

--- app/exports/urls.py
+++ app/exports/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('measurements', views.export_to_tsv, name='tsv'),
+    path('ETS', views.export_to_tsv, name='ets'),
     path('get_file/<int:file_id>', views.get_exported_file, name='getfile'),
     path('submitted', views.form_submitted, name='submission')
 ]
