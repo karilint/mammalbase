7392de789ddcc8878062a5c193077bf49b117d9e
masiro918 <matias.siro@outlook.com>
2024-02-01 14:29:26 +0200

    Fixed ;
    Co-authored-by: A00N <nsaareks@gmail.com>

--- app/imports/importers/occurrence_importer.py
+++ app/imports/importers/occurrence_importer.py
@@ -14,8 +14,8 @@ class OccurrencesImporter(BaseImporter):
         #author = self.get_author(getattr(row, 'author')) # toimii jos tyyliin 
         author = "kissa" #self.get_author(getattr(row, 'author'))
         reference = self.get_or_create_source_reference(getattr(row, 'references'), author)
-        entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
-        taxon = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
+        entityclass = None #self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
+        taxon = None #self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
         verbatimScientificname = taxon
         
         column_functions = {
@@ -26,20 +26,22 @@ class OccurrencesImporter(BaseImporter):
             "organism_quantity" : getattr(row, 'organismQuantity'),
             "organism_quantity_type" : getattr(row, 'organismQuantityType'),
             "verbatim_scientific_name" : verbatimScientificname,          
-            "gender" : ChoiceValue(getattr(row, 'sex')),
-            "life_stage" : ChoiceValue(getattr(row, 'lifeStage')),
+            "gender" : self.get_choicevalue(getattr(row, 'sex')),
+            "life_stage" : self.get_choicevalue(getattr(row, 'lifeStage')),
             "occurrence_remarks" : getattr(row, 'occurrenceRemarks'),
             "associated_references" : getattr(row, 'associatedReferences'),
         }
         
+        
         # Dictionary comprehension for conditional assignments
         row_data = {key: func(getattr(row, key)) for key, func in column_functions.items() if key in headers}
 
+        
         # Ensure default values for keys not in headers
         default_values = {'location': None, 'gender': None, 'sample_size': 0, 'cited_reference': None, 'time_period': None, 'method': None, 'study_time': None}
         row_data = {**default_values, **row_data}
 
-
+        """
         # Creating or retrieving DietSet object
         ds_kwargs = {'reference': reference, 'taxon': taxon, 'created_by': author, **row_data}
         
@@ -56,15 +58,14 @@ class OccurrencesImporter(BaseImporter):
         for key, value in model_attribute_mapping.items():
             if key in ds_kwargs:
                 ds_kwargs[value] = ds_kwargs.pop(key)
+        """
         
-        ds, created = Occurrence.objects.get_or_create(**ds_kwargs)
+        ds, created = Occurrence.objects.get_or_create(**column_functions)
         if created:
             print("New  created:", ds)
         else:
             print("Existing DietSet found:", ds)
         
-        new_occurrence = Occurrence(**column_functions)
-        new_occurrence.save()
     
 
 
