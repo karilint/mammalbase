c0aca9204195c631a719f08b6bafdf8e7cd6ecfe
masiro918 <matias.siro@outlook.com>
2024-02-01 15:48:10 +0200

    Delete migrations

--- app/imports/importers/occurrence_importer.py
+++ app/imports/importers/occurrence_importer.py
@@ -14,8 +14,8 @@ class OccurrencesImporter(BaseImporter):
         #author = self.get_author(getattr(row, 'author')) # toimii jos tyyliin 
         author = "kissa" 
         reference = self.get_or_create_source_reference(getattr(row, 'references'), author)
-        entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
-        verbatimScientificname = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
+        #entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
+        #verbatimScientificname = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
          
         
         column_functions = {
@@ -25,46 +25,20 @@ class OccurrencesImporter(BaseImporter):
             "source_entity" : None,
             "organism_quantity" : getattr(row, 'organismQuantity'),
             "organism_quantity_type" : getattr(row, 'organismQuantityType'),
-            "verbatim_scientific_name" : verbatimScientificname,          
+            #"verbatim_scientific_name" : None,          
             "gender" : self.get_choicevalue(getattr(row, 'sex')),
             "life_stage" : self.get_choicevalue(getattr(row, 'lifeStage')),
             "occurrence_remarks" : getattr(row, 'occurrenceRemarks'),
             "associated_references" : getattr(row, 'associatedReferences'),
         }
         
-        
-        # Dictionary comprehension for conditional assignments
-        row_data = {key: func(getattr(row, key)) for key, func in column_functions.items() if key in headers}
 
         
-        # Ensure default values for keys not in headers
-        default_values = {'location': None, 'gender': None, 'sample_size': 0, 'cited_reference': None, 'time_period': None, 'method': None, 'study_time': None}
-        row_data = {**default_values, **row_data}
-
-        """
-        # Creating or retrieving DietSet object
-        ds_kwargs = {'reference': reference, 'taxon': taxon, 'created_by': author, **row_data}
-        
-        # Rename keys to match model attributes
-        model_attribute_mapping = {
-            'sex': 'gender',
-            'individualCount': 'sample_size',
-            'associatedReferences': 'cited_reference',
-            'samplingEffort': 'time_period',
-            'measurementMethod': 'method',
-            'verbatimEventDate': 'study_time',
-            'verbatimLocality': 'location',
-        }
-        for key, value in model_attribute_mapping.items():
-            if key in ds_kwargs:
-                ds_kwargs[value] = ds_kwargs.pop(key)
-        """
-        
-        ds, created = Occurrence.objects.get_or_create(**column_functions)
+        created = Occurrence.objects.get_or_create(**column_functions)
         if created:
-            print("New  created:", ds)
+            print("New  created:")
         else:
-            print("Existing DietSet found:", ds)
+            print("Existing DietSet found:")
         
     
 
