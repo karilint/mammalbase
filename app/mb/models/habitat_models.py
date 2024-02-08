1c804da1d9bb0fcf9a839222fa4c40e006c91e0b
ElliJohansson <elli.johansson@live.com>
2024-02-08 13:41:25 +0200

    create initial MasterHabitat and HabitatRelation models
    Co-authored-by: Patrick Salmi <patrick.salmi@helsinki.fi>
    Co-authored-by: Sanni Tuomisto <sanni.tuomisto@helsinki.fi>

--- app/mb/models/habitat_models.py
+++ app/mb/models/habitat_models.py
@@ -5,6 +5,11 @@ class SourceHabitat(BaseModel):
     """
     Model representing a habitat associated with an Occurrence.
     """
+    master_habitat = models.ManyToManyField(
+        'MasterHabitat',
+        through='habitatRelation',
+        through_fields=('source_habitat, master_habitat')
+    )
     source_reference = models.ForeignKey(
         'SourceReference',
         on_delete = models.CASCADE,
@@ -23,5 +28,51 @@ class SourceHabitat(BaseModel):
         )
     
 class MasterHabitat(BaseModel):
-    pass
+    reference = models.ForeignKey(
+        'MasterReference',
+        on_delete = models.CASCADE,
+        )
+    source_habitat = models.ManyToManyField(
+        'SourceHabitat',
+        through='HabitatRelation',
+        through_fields=('master_habitat, source_habitat')
+        )
+    name = models.CharField(
+        max_length=250, 
+        help_text="Enter the Name of the Master Habitat"
+        )
+    
+    class Meta:
+        ordering = ['name']
 
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular Source Habitat instance.
+        """
+        return reverse('master-habitat-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return '%s' % (self.name)
+    
+
+class HabitatRelation(BaseModel):
+    source_habitat = models.ForeignKey('SourceHabitat', on_delete=models.CASCADE)
+    master_habitat = models.ForeignKey('MasterHabitat', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('source_habitat', 'master_habitat',)
+
+        def get_absolute_url(self):
+            """
+            Returns the url to access a particular Source Habitat instance.
+            """
+            return reverse('attribute-relation-detail', args=[str(self.id)])
+
+        def __str__(self):
+            """
+            String for representing the Model object
+            """
+            return '{0} ({1}) {2}'.format(self.source_habitat.habitat_type,self.master_habitat.name,self.master_habitat.reference)
