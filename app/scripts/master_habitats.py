9aac2b5c17b1bb90b07123bf0b19ef0493cf189c
PatrickSalmi <patrick.salmi@helsinki.fi>
2024-02-14 14:11:19 +0200

    Refactor olson script
    Co-authored-by: ElliJohansson <elli.johansson@live.com>
    Co-authored-by: Sanni Tuomisto <sanni.tuomisto@helsinki.fi>

--- app/scripts/master_habitats.py
+++ app/scripts/master_habitats.py
@@ -7,42 +7,36 @@ from mb.models.habitat_models import MasterHabitat
 from imports.importers.base_importer import BaseImporter
 from django.contrib.auth.models import User
 
+importer = BaseImporter()
 
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-
 user = User.objects.get(username=username)
-importer = BaseImporter()
-reference = importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=user)
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-csv_path = os.path.join(script_dir, "biomes_olson_1983.csv")
 
-df = pd.read_csv(csv_path)
+olson_reference = importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=user)
+olson_path = os.path.join(script_dir, "biomes_olson_1983.csv")
+olson_df = pd.read_csv(olson_path)
 
-for index, row in df.iterrows():
+for index, row in olson_df.iterrows():
     prev_habitat = None
     for i in range(len(row)-1):
         name = row.iloc[i]
         eco_code = row.iloc[-1]
         if pd.notna(name) and name.strip():
             habitat, created = MasterHabitat.objects.get_or_create(
-                reference=reference, 
-                eco_code=eco_code, 
-                name=name, 
+                reference=olson_reference,
+                eco_code=eco_code,
+                name=name,
             )
-            
+
         if created:
             habitat.parent = prev_habitat
             habitat.save()
-        elif habitat.parent is None and prev_habitat is not None:
-            habitat.parent = prev_habitat
-            habitat.save()
-        
-            
-        prev_habitat = habitat
-        
-    #for col in other_columns:
-    #    print(row[col])
 
-    #habitat = MasterHabitat(reference=reference,eco_code=eco_code, name=name)
-    #habitat.save()
+        prev_habitat = habitat
+
+wwf_reference = None
+wwf_path = os.path.join(script_dir, "biomes_olson_1983.csv")
+wwf_df = pd.read_csv(wwf_path)
+
