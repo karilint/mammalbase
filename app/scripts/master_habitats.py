248c70b452718cc6316f9d44f7b8ea253c20fcac
PatrickSalmi <patrick.salmi@helsinki.fi>
2024-02-14 14:59:28 +0200

    add master habitat csv files
    Co-authored-by: ElliJohansson <elli.johansson@live.com>
    Co-authored-by: Sanni Tuomisto <sanni.tuomisto@helsinki.fi>

--- app/scripts/master_habitats.py
+++ app/scripts/master_habitats.py
@@ -12,11 +12,10 @@ importer = BaseImporter()
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 user = User.objects.get(username=username)
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
+csv_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'habitat_csv')
 
 olson_reference = importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=user)
-olson_path = os.path.join(script_dir, "biomes_olson_1983.csv")
+olson_path = os.path.join(csv_dir, "biomes_olson_1983.csv")
 olson_df = pd.read_csv(olson_path)
 
 for index, row in olson_df.iterrows():
@@ -37,36 +36,35 @@ for index, row in olson_df.iterrows():
 
         prev_habitat = habitat
 
-
-wwf_path = os.path.join(script_dir, "biomes_wwf.csv")
+wwf_path = os.path.join(csv_dir, "biomes_wwf.csv")
 wwf_df = pd.read_csv(wwf_path)
 
 for index, row in wwf_df.iterrows():
     biome_code = row.iloc[0]
     name = row.iloc[1]
-    
+
     habitat, created = MasterHabitat.objects.get_or_create(
                 reference=olson_reference,
                 biome_code=biome_code,
                 name=name,
             )
-    
+
     if created:
         habitat.save()
 
 holdridge_reference = importer.get_or_create_master_reference(citation="Holdridge, L.R. (1947). Determination of world plant formations from simple climatic data. Science. 105 (2727): 367–8.", author=user)       
-holdridge_path = os.path.join(script_dir, "biomes_holdridge.csv")
+holdridge_path = os.path.join(csv_dir, "biomes_holdridge.csv")
 holdridge_df = pd.read_csv(holdridge_path)
 
 for index, row in holdridge_df.iterrows():
     iiasa_code = row.iloc[0]
     name = row.iloc[1]
-    
+
     habitat, created = MasterHabitat.objects.get_or_create(
                 reference=holdridge_reference,
                 iiasa_code=iiasa_code,
                 name=name,
             )
-    
+
     if created:
         habitat.save()
