5334a3a8a3c5821f295dbbf9978ebd8c6c6fcda8
masiro918 <matias.siro@outlook.com>
2024-02-15 15:12:30 +0200

    Change ets to diet set

--- app/imports/validation_lib/diet_set_validation.py
+++ app/imports/validation_lib/diet_set_validation.py
@@ -7,60 +7,43 @@ class Diet_set_validation(Validation):
         super().__init__()
 
         self.rules = {
-            "references": "required|min:10|max:500|regex:.*([1-2][0-9]{3})", #DONE #|in_db:SourceReference,citation__iexact
-            "verbatimScientificName": "required|alpha", #Done
-            "scientificNameAuthorship": "", #Done #nameyear ennen
-            "taxonRank": "in:Subspecies,Varietas,Forma,Species,Genus,Nothogenus,Nothospecies,Nothosubspecies,Family,nan", #DONE
-            "organismQuantity": "digits", #DONE
-            "organismQuantityType": "", #DONE, no possible validator since darvincore quote "whereas this term allows for any string literal value." e.g '% biomass'
-            "sex": "in:male,female,nan", #DONE
-            "lifeStage": "alpha", #Only accept alphabeticals #alpha
-            "verbatimEventDate": "", #DONE
-            #"verbatimEventDate": "in: I,V,X,L,C,D,M,1,2,3,4,5,6,7,8,9,nan", #DONE
-            "occurrenceRemarks": "", #DONE, cannot be tested since it can be anything
-            "verbatimLocality": "", #DONE, cannot be tested since it can be anything
-            "verbatimElevation": "", #DONE
-            "verbatimDepth": "", #DONE
-            #"verbatimCoordinates": "in:N,E,S,W,°,T,d,nan", #DONE
-            "verbatimCoordinates": "verbatimCoordinates", #DONE
-            "verbatimLatitude": "", #DONE
-            "verbatimLongitude": "", #DONE
-            #"verbatimLatitude": "in:N,E,S,W,°,T,d,nan", #DONE
-            #"verbatimLongitude": "in:N,E,S,W,°,T,d,nan", #DONE
-            "verbatimCoordinateSystem": "in:decimal degrees,degrees minutes,degrees decimal seconds,UTM,nan", #Done
-            "verbatimSRS": "", #Maybe done maybe kesken
-            "author": "required|author", #DONE
-            "associatedReferences": "", #DONE, can be anything
-            "samplingProtocol": "", #DONE, no possible validator since darincore quote "whereas this term allows for any string literal value." e.g 'Takats et al. 2001. Guidelines for Nocturnal Owl Monitoring in North America. Beaverhill Bird Observatory and Bird Studies Canada, Edmonton, Alberta. 32 pp., http://www.bsc-eoc.org/download/Owl.pdf'
-            "habitatType": "", #DONE, no possible validator since darincore quote "whereas this term allows for any string literal value." e.g 'B (bushland): densely growing woody vegetation of shrubby habit, low stature <6 m in height, canopy cover >20%'
-            "habitatPercentage": "" #Done
+            "author":                       "required|author", #DONE
+            "verbatimScientificName":       "required|alpha", #Done
+            "taxonRank":                    "in:Subspecies,Varietas,Forma,Species,Genus,Nothogenus,Nothospecies,Nothosubspecies,Family,nan", #DONE
+            "verbatimLocality":             "max:250", #DONE, cannot be tested since it can be anything
+            "habitat" :                     "max:250", # ???
+            "samplingEffort" :              "max:250",
+            "sex":                          "in:male,female,nan", #DONE
+            "individualCount" :             "digits",
+            "verbatimEventDate":            "max:250", #DONE
+            "measurementMethod " :          "max:500", #max 500
+            "verbatimAssociatedTaxa" :      "max:250", #DONE alkaa verbatim
+            "PartOfOrganism" :              "in:BARK,BLOOD,BONES,BUD,CARRION,EGGS,EXUDATES,FECES,FLOWER,FRUIT,LARVAE,LEAF,MINERAL,NECTAR/JUICE,NONE,POLLEN,ROOT,SEED,SHOOT,STEM,UNKNOWN,WHOLE",
+            "sequence" :                    "max:250", #?
+            "measurementValue" :            "digis|min:0", #?
+            "associatedReferences" :        "max:250", #max 250
+            "references":                   "required|min:10|max:500|regex:.*([1-2][0-9]{3})", #DONE #|in_db:SourceReference,citation__iexact
         }
+
     
         self.data = {
-                "references": "",
-                "verbatimScientificName": "",
-                "scientificNameAuthorship": "",
-                "taxonRank": "",
-                "organismQuantity": "",
-                "organismQuantityType": "",
-                "sex": "",
-                "lifeStage": "",
-                "verbatimEventDate": "",
-                "occurrenceRemarks": "",
-                "verbatimLocality": "",
-                "verbatimElevation": "",
-                "verbatimDepth": "",
-                "verbatimCoordinates": "",
-                "verbatimLatitude": "",
-                "verbatimLongitude": "",
-                "verbatimCoordinateSystem": "",
-                "verbatimSRS": "",
-                "author": "",
-                "associatedReferences": "",
-                "samplingProtocol": "",
-                "habitatType": "",
-                "habitatPercentage": ""
-            }
+            "author": "",
+            "verbatimScientificName": "",
+            "taxonRank": "",
+            "verbatimLocality": "",
+            "habitat": "",
+            "samplingEffort": "",
+            "sex": "",
+            "individualCount": "",
+            "verbatimEventDate": "",
+            "measurementMethod": "",
+            "verbatimAssociatedTaxa": "",
+            "PartOfOrganism": "",
+            "sequence": "",
+            "measurementValue": "",
+            "associatedReferences": "",
+            "references": ""
+        }
     
 
 
