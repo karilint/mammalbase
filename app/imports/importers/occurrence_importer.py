e67b0a6d4f1e4091afb13bb6bfc95ac04d2404ae
masiro918 <matias.siro@outlook.com>
2024-02-21 13:50:14 +0200

    Add validation for life stage

--- app/imports/importers/occurrence_importer.py
+++ app/imports/importers/occurrence_importer.py
@@ -22,40 +22,39 @@ class OccurrencesImporter(BaseImporter):
         """
         
         # Common assignments
-        try:
-            author = self.get_author(getattr(row, 'author'))
-            reference = self.get_or_create_source_reference(getattr(row, 'references'), author)
-            entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
-            verbatimScientificname = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
-            
-            
-            created = None
-            
-            # Create source location model
-            new_source_location = self.get_or_create_source_location(getattr(row, 'verbatimLocality'), reference, author)
-            new_event, created = Event.objects.get_or_create(verbatim_event_date=getattr(row, 'verbatimEventDate'))
-            
-            gender = str(getattr(row, 'sex'))
-            life_stage = str(getattr(row, 'lifeStage'))
+        author = self.get_author(getattr(row, 'author'))
+        reference = self.get_or_create_source_reference(getattr(row, 'references'), author)
+        entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
+        verbatimScientificname = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
+        
+        
+        created = None
+        
+        # Create source location model
+        new_source_location = self.get_or_create_source_location(getattr(row, 'verbatimLocality'), reference, author)
+        new_event, created = Event.objects.get_or_create(verbatim_event_date=getattr(row, 'verbatimEventDate'))
+        
+        gender = str(getattr(row, 'sex'))
+        life_stage = str(getattr(row, 'lifeStage'))
 
-            if gender == "nan":
-                gender = None
-            else:
-                gender, created = ChoiceValue.objects.get_or_create(choice_set="Gender", caption=gender.capitalize())
+        if gender == "nan":
+            gender = None
+        else:
+            gender, created = ChoiceValue.objects.get_or_create(choice_set="Gender", caption=gender.capitalize())
 
-            if life_stage == "nan":
-                life_stage = None
-            else:
-                life_stage, created = ChoiceValue.objects.get_or_create(choice_set="Lifestage", caption=life_stage.capitalize())
-            
-            obj, created = Occurrence.objects.get_or_create(source_reference=reference, event=new_event, source_location=new_source_location, source_entity=verbatimScientificname,
-                                                        organism_quantity=getattr(row, 'organismQuantity'), organism_quantity_type=getattr(row, 'organismQuantityType'), gender=gender, 
-                                                        life_stage=life_stage,
-                                                        occurrence_remarks=getattr(row, 'occurrenceRemarks'), associated_references=getattr(row, 'associatedReferences'))
-            if created:
-                return True
-            else:
-                return False
+        if life_stage == "nan":
+            life_stage = None
+        else:
+            life_stage, created = ChoiceValue.objects.get_or_create(choice_set="Lifestage", caption=life_stage.capitalize())
+        
+        obj, created = Occurrence.objects.get_or_create(source_reference=reference, event=new_event, source_location=new_source_location, source_entity=verbatimScientificname,
+                                                    organism_quantity=getattr(row, 'organismQuantity'), organism_quantity_type=getattr(row, 'organismQuantityType'), gender=gender, 
+                                                    life_stage=life_stage,
+                                                    occurrence_remarks=getattr(row, 'occurrenceRemarks'), associated_references=getattr(row, 'associatedReferences'))
+        if created:
+            return True
+        else:
+            return False
      
     
 
