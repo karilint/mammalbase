ad37b091df5d0f91f47a91ec1a68fa024860cbfd
Patrick Salmi <patrick.salmi@helsinki.fi>
2024-02-22 14:33:14 +0200

    Refactor master_habitat_script
    Co-authored-by: ElliJohansson <elli.johansson@live.com>

--- app/mb/management/commands/master_habitat_script.py
+++ app/mb/management/commands/master_habitat_script.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import os
 import pandas as pd
 from mb.models.habitat_models import MasterHabitat
@@ -6,19 +7,26 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        importer = BaseImporter()
-
+    def __init__(self):
+        self.importer = BaseImporter()
+        self.csv_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'habitat_csv')
         username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-        user = User.objects.get(username=username)
+        self.user = User.objects.get(username=username)
+        
+    def handle(self, *args, **options):
+        print("Adding Olson biomes...")
+        self.add_olson_biomes()
+        print("Adding WWF biomes...")
+        self.add_wwf_biomes()
+        print("Adding Holdridge biomes...")
+        self.add_holdridge_biomes()
 
-        csv_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'habitat_csv')
-
-        wwf_reference = importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=user)
-    
-        #olson_reference = importer.get_or_create_master_reference(citation="Olson, J S, Watts, J A, and Allison, L J. Carbon in live vegetation of major world ecosystems. United States: N. p., 1983. Web.", author=user)
-        olson_path = os.path.join(csv_dir, "biomes_olson_1983.csv")
+        print("Master Habitats created successfully")
+        
+        
+    def add_olson_biomes(self):
+        olson_reference = self.importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=self.user)
+        olson_path = os.path.join(self.csv_dir, "biomes_olson_1983.csv")
         olson_df = pd.read_csv(olson_path)
 
         habitats = {}
@@ -38,7 +46,7 @@ class Command(BaseCommand):
                         if key not in habitats:
                             habitat = MasterHabitat.objects.create(
                                 name=name,
-                                reference=wwf_reference,
+                                reference=olson_reference,
                                 parent=prev_habitat,
                                 code=eco_code,
                                 group=header
@@ -50,9 +58,10 @@ class Command(BaseCommand):
                     prev_habitat = habitat
                 else:
                     prev_habitat = None
-
-        #wwf_reference = importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=user)
-        wwf_path = os.path.join(csv_dir, "biomes_wwf.csv")
+    
+    def add_wwf_biomes(self):
+        wwf_reference = self.importer.get_or_create_master_reference(citation="Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N., Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J., Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem, K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth. Bioscience 51(11):933-938.", author=self.user)
+        wwf_path = os.path.join(self.csv_dir, "biomes_wwf.csv")
         wwf_df = pd.read_csv(wwf_path)
 
         for index, row in wwf_df.iterrows():
@@ -67,9 +76,10 @@ class Command(BaseCommand):
 
             if created:
                 habitat.save()
-
-        holdridge_reference = importer.get_or_create_master_reference(citation="Holdridge, L.R. (1947). Determination of world plant formations from simple climatic data. Science. 105 (2727): 367–8.", author=user)       
-        holdridge_path = os.path.join(csv_dir, "biomes_holdridge.csv")
+    
+    def add_holdridge_biomes(self):
+        holdridge_reference = self.importer.get_or_create_master_reference(citation="Holdridge, L.R. (1947). Determination of world plant formations from simple climatic data. Science. 105 (2727): 367–8.", author=self.user)       
+        holdridge_path = os.path.join(self.csv_dir, "biomes_holdridge.csv")
         holdridge_df = pd.read_csv(holdridge_path)
 
         for index, row in holdridge_df.iterrows():
@@ -84,5 +94,3 @@ class Command(BaseCommand):
 
             if created:
                 habitat.save()
-
-        print("Master Habitats created successfully")
