93e15d62d029ad00e9cb85ccac87f778984ceb44
Ronja Tuovinen <ronja.tuovinen@helsinki.fi>
2024-02-23 18:03:52 +0200

    difflib matchtoolissa

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,8 +1,12 @@
+import difflib
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 
+
 @login_required
 #@permission_required('matchtool.trait_match', raise_exception=True)
-def trait_match(request):
+def trait_match(request, trait): #trait is the trait that gets matched
     #TODO
+    
+    closest = difflib.get_close_matches(trait, traits, 1, 0.5)  #returns the one best match if the possibility is over 0.5
     return render(request, 'matchtool/trait_match.html')
