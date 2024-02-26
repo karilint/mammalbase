8adb1e27463226c0b88761a50b265f299a1b119c
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-02-26 15:56:25 +0200

    find exact match

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
+from mb.models.models import SourceAttribute, AttributeRelation
+from django.db.models import Q
 
 @login_required
-#@permission_required('matchtool.trait_match', raise_exception=True)
+# @permission_required('matchtool.trait_match', raise_exception=True)
 def trait_match(request):
-    #TODO
+    """Match source attributes to master attributes."""
+    unlinked_attributes = SourceAttribute.objects.filter(
+        Q(master_attribute=None) | Q(master_attribute__name='- Checked, Unlinked -'))
+
+    relations = AttributeRelation.objects.exclude(
+        master_attribute__name='- Checked, Unlinked -')
+
+    for source_attribute in unlinked_attributes:
+        master_attribute = find_exact_match(source_attribute, relations)
+        if master_attribute:
+            print(source_attribute.name, master_attribute.name)
+
     return render(request, 'matchtool/trait_match.html')
+
+
+def find_exact_match(source, relations):
+    """Check if source attribute has an exact match in the relations."""
+    for relation in relations:
+        if source.name.lower() == relation.source_attribute.name.lower():
+            return relation.master_attribute
