b2a69649d7abfae2c063f196ebd0d6cd2fa5b827
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-03-06 10:05:46 +0200

    fix models

--- app/matchtools/models.py
+++ app/matchtools/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from mb.models import BaseModel, MasterAttribute
 
 class WordCount(BaseModel):
     word = models.CharField(max_length=200)
-    master_attribute = models.ForeignKey('MasterAttribute', on_delete=models.CASCADE)
+    master_attribute = models.ForeignKey(MasterAttribute, on_delete=models.CASCADE)
     count = models.IntegerField()
-
+    
 class ProbabilityTable(BaseModel):
-    word = models.ForeignKey('Word', on_delete=models.CASCADE)
-    master_attribute = models.ForeignKey('MasterAttribute', on_delete=models.CASCADE)
+    word = models.ForeignKey(WordCount, on_delete=models.CASCADE)
+    master_attribute = models.ForeignKey(MasterAttribute, on_delete=models.CASCADE)
     probability = models.IntegerField()
