d8f323a97bf95bef4804aa2297cf651b89695a14
Ronja Tuovinen <ronja.tuovinen@helsinki.fi>
2024-03-06 11:27:26 +0200

    loop done, not tested

--- app/matchtools/models.py
+++ app/matchtools/models.py
@@ -3,10 +3,10 @@ from mb.models import BaseModel, MasterAttribute
 
 class WordCount(BaseModel):
     word = models.CharField(max_length=200)
-    master_attribute = models.ForeignKey(MasterAttribute, on_delete=models.CASCADE)
+    #master_attribute = models.ForeignKey(MasterAttribute, on_delete=models.CASCADE)
     count = models.IntegerField()
     
-class ProbabilityTable(BaseModel):
-    word = models.ForeignKey(WordCount, on_delete=models.CASCADE)
-    master_attribute = models.ForeignKey(MasterAttribute, on_delete=models.CASCADE)
-    probability = models.IntegerField()
+# class ProbabilityTable(BaseModel):
+#     word = models.ForeignKey(WordCount, on_delete=models.CASCADE)
+#     master_attribute = models.ForeignKey(MasterAttribute, on_delete=models.CASCADE)
+#     probability = models.IntegerField()
--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,8 +1,8 @@
 import difflib
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
-from app.matchtools.models import WordCount
-from mb.models.models import SourceAttribute, AttributeRelation
+from .models import WordCount
+from mb.models import SourceAttribute, AttributeRelation
 from django.db.models import Q
 from fuzzywuzzy import fuzz, process
 import time
@@ -21,17 +21,24 @@ def trait_match(request):
                                      for item in relations], score_cutoff=80, scorer=fuzz.token_set_ratio)
         
     for attribute in relations: # Tällä käydään läpi olemassaolevat relaatiot ja luodaan sanataulukko
-        master = attribute['master_attribute__mane']
-        sources = attribute['source_attribute__name'].split(" ")
+        master = attribute['master_attribute__name']
+        sources = attribute['source_attribute__name'].split()
         for source in sources:
-            joku = WordCount.objects.filter(
-                Q(word = source)
+            found_word = WordCount.objects.filter(
+                Q(word = source) & Q(master_attribute = master)
             )
-            if joku != None: 
+            if found_word == None: #jos sanaa ei löydy luodaan uusi solu siihen kohtaan TODO: tee jotain "tyhjille" soluille, kun lasketaan todennäköisyydet
                 WordCount.object.create(
                     word = source,
-                    master_attribute = master
+                    master_attribute = master,
+                    count = 1
                 )
+            else: #Jos sana löytyy, lisätään sanojen määrää yhdellä
+                current_count = found_word['count']
+                WordCount.objects.filter(
+                Q(word = source) & Q(master_attribute = master)
+            ).update(count = current_count + 1)
+
 
         if matches:
             print(source.name, matches[0], matches[1])
