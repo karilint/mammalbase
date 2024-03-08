a908daa72d429b8077558dafa47e46c4b5a65b3e
Ohjelmistotuotantoprojekti <github-hy-ohtuprojekti@v.hix.fi>
2024-03-08 19:23:17 +0200

    diet urls as own submodule

--- app/mb/urls/__init__.py
+++ app/mb/urls/__init__.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('mb.urls.main')),
+    path('', include('mb.urls.diet')),
     path('', include('mb.urls.debug_toolbar')),
     path('', include('mb.urls.unsorted')),
     path('exports/', include('exports.urls')),
