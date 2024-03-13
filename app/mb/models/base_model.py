4dfe10e18c80eeb8ab2a91e2195a858e802be4d5
Ohjelmistotuotantoprojekti <github-hy-ohtuprojekti@v.hix.fi>
2024-03-13 13:46:26 +0200

    base_model lint

--- app/mb/models/base_model.py
+++ app/mb/models/base_model.py
@@ -15,7 +15,8 @@ class ActiveManager(models.Manager):
     def get_queryset(self):
         return CustomQuerySet(self.model, using=self._db)
 
-# https://medium.com/@KevinPavlish/add-common-fields-to-all-your-django-models-bce033ac2cdc
+# https://medium.com/@KevinPavlish/
+# add-common-fields-to-all-your-django-models-bce033ac2cdc
 class BaseModel(models.Model):
     """
     A base model including basic fields for each Model
@@ -23,35 +24,49 @@ class BaseModel(models.Model):
     """
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
-    created_by = UserForeignKey(auto_user_add=True, verbose_name="The user that is automatically assigned", related_name='createdby_%(class)s')
-    modified_by = UserForeignKey(auto_user=True, verbose_name="The user that is automatically assigned", related_name='modifiedby_%(class)s')
-# https://django-simple-history.readthedocs.io/en/2.6.0/index.html
+    created_by = UserForeignKey(
+            auto_user_add=True,
+            verbose_name="The user that is automatically assigned",
+            related_name='createdby_%(class)s')
+    modified_by = UserForeignKey(
+            auto_user=True,
+            verbose_name="The user that is automatically assigned",
+            related_name='modifiedby_%(class)s')
+    # https://django-simple-history.readthedocs.io/en/2.6.0/index.html
     history = HistoricalRecords(
-        history_change_reason_field=models.TextField(null=True),
-        inherit=True)
-# https://stackoverflow.com/questions/5190313/django-booleanfield-how-to-set-the-default-value-to-true
-    is_active = models.BooleanField(default=True, help_text='Is the record active')
+            history_change_reason_field=models.TextField(null=True),
+            inherit=True)
+    # https://stackoverflow.com/questions/5190313/
+    # django-booleanfield-how-to-set-the-default-value-to-true
+    is_active = models.BooleanField(
+            default=True,
+            help_text='Is the record active')
     objects = ActiveManager()
 
-# https://stackoverflow.com/questions/4825815/prevent-delete-in-django-model
-#    def delete(self):
-#        self.is_active = False
-#        self.save()
+    # https://stackoverflow.com/questions/4825815/
+    # prevent-delete-in-django-model
+    #    def delete(self):
+    #        self.is_active = False
+    #        self.save()
 
     # Code by ChatGPT4
     def delete(self):
         """
-        Recursively soft delete this object and cascade the soft delete to related objects.
+        Recursively soft delete this object and cascade the soft delete to
+        related objects.
         """
         # Handle related fields with cascading deletion
         for rel in self._meta.related_objects:
-            if rel.on_delete == models.CASCADE and hasattr(rel.related_model, 'is_active'):
+            if (rel.on_delete == models.CASCADE
+                    and hasattr(rel.related_model, 'is_active')):
                 related_manager = getattr(self, rel.get_accessor_name())
                 related_objects = related_manager.all()
 
                 # Recursively soft delete the related objects
                 for related_obj in related_objects:
-                    related_obj.delete()  # This will ensure related objects of related_obj are also soft-deleted
+                    # This will ensure related objects of related_obj are
+                    # also soft-deleted
+                    related_obj.delete()
 
         # Soft delete the current object
         self.is_active = False
