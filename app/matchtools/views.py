a39e22146920ee09ad499dc09b50a70791773d27
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-03-13 11:55:15 +0200

    refine queries

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
 # from .models import WordCount
-from mb.models import SourceAttribute, AttributeRelation
+from mb.models import SourceAttribute, AttributeRelation, MasterAttribute
 from django.db.models import Q
 from fuzzywuzzy import fuzz, process
+import re
 
 
 @login_required
 # @permission_required('matchtool.trait_match', raise_exception=True)
 def trait_match(request):
     """Match source attributes to master attributes."""
-    relations = AttributeRelation.objects.exclude(
-        master_attribute__is_active=False
-    ).select_related('source_attribute', 'master_attribute').values('source_attribute__name', 'master_attribute__name')
+    relations = MasterAttribute.objects.exclude(is_active=False).values_list('name', 'source_attribute__name')
 
     for source in SourceAttribute.objects.filter(master_attribute=None):
-        matches = process.extractOne(source.name, [(item['source_attribute__name'], item['master_attribute__name'])
-                                     for item in relations], score_cutoff=80, scorer=fuzz.token_set_ratio)
-        
+        matches = process.extractOne(source.name, relations, scorer=fuzz.token_set_ratio, score_cutoff=75)
         if matches:
-            print(source.name, matches[0], matches[1])
+            print(source.name,matches)
+            
     # for attribute in relations: # Tällä käydään läpi olemassaolevat relaatiot ja luodaan sanataulukko
     #     master = attribute['master_attribute__name']
     #     sources = attribute['source_attribute__name'].split()
