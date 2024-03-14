be146f6f36a5e429cff9aca11b4264230f7af19c
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2024-03-14 12:13:04 +0200

    Fix location matchtool

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -4,10 +4,10 @@ from .location_api import LocationAPI
 
 def location_matchtool(request):
     result = []
+    sources = []
     api = LocationAPI()
     if request.method == 'POST':
         query = request.POST.get('query')
         result = api.get_master_location(query)
         sources = SourceLocation.objects.filter(name__icontains=query)
-        print(sources)
     return render(request, 'location_matchtool.html', {'results': result, 'sources': sources})
