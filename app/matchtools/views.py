5e2cbd049df96debbbcb84701f710eacf49eb441
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-03-15 14:06:32 +0200

    functional form and logic

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,32 +1,65 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponseRedirect
 #from .models import WordCount
 from .forms import AttributeRelationForm
-from mb.models import SourceAttribute, MasterAttribute
+from mb.models import SourceAttribute, MasterAttribute, AttributeRelation
 from django.db.models import Q
 from fuzzywuzzy import fuzz, process
+
 
 @login_required
 def info_traitmatch(request):
     """Match tool info page."""
-    unmatched_source = SourceAttribute.objects.filter(master_attribute=None).first()
+    unmatched_source = get_unmatched()
     return render(request, 'matchtool/info_trait_match.html', {'unmatched_source': unmatched_source})
 
-@login_required 
+
+@login_required
 @permission_required('matchtool.trait_match', raise_exception=True)
 def trait_match(request, source_attribute_id):
     """Match source attributes to master attributes."""
-    unmatched_source = get_object_or_404(SourceAttribute, pk=source_attribute_id)
-    form = AttributeRelationForm(instance=unmatched_source)
-    
-    relations = MasterAttribute.objects.exclude(is_active=False).values_list('name', 'source_attribute__name')[:20]
+    unmatched_source = get_object_or_404(
+        SourceAttribute, pk=source_attribute_id)
+    found_match = get_match(unmatched_source.name)
+    if not found_match:
+        return redirect('info-trait-match')
 
+    master = MasterAttribute.objects.get(name=found_match)
+    attribute_relation = AttributeRelation(
+        source_attribute=unmatched_source, master_attribute=master)
+    form = AttributeRelationForm(instance=attribute_relation)
+
+    if request.method == 'POST':
+        form = AttributeRelationForm(request.POST, instance=attribute_relation)
+        if form.is_valid():
+            form.save()
+            next_source = get_unmatched()
+            if next_source:
+                return redirect('trait-match', source_attribute_id=next_source.id)
+            else:
+                return redirect('info-trait-match')
+
+    return render(request, 'matchtool/trait_match.html', {'form': form})
+
+
+def get_unmatched():
+    """Get first unmatched source attribute."""
     for source in SourceAttribute.objects.filter(master_attribute=None):
-        matches = process.extractOne(source.name, relations, scorer=fuzz.token_set_ratio, score_cutoff=75)
-        if matches:
-            print(source.name,matches)
-            
+        match = get_match(source.name)
+        if match:
+            return source
+    return None
+
+
+def get_match(source_name):
+    """Get best match for source attribute from master attributes using fuzzy matching."""
+    relations = MasterAttribute.objects.exclude(
+        is_active=False).values_list('name', 'source_attribute__name')
+    match = process.extractOne(
+        source_name, relations, scorer=fuzz.token_set_ratio, score_cutoff=75)
+    return match[0][0] if match else None
+
     # for attribute in relations: # Tällä käydään läpi olemassaolevat relaatiot ja luodaan sanataulukko
     #     master = attribute['master_attribute__name']
     #     sources = attribute['source_attribute__name'].split()
@@ -45,16 +78,3 @@ def trait_match(request, source_attribute_id):
     #             WordCount.objects.filter(
     #             Q(word = source) & Q(master_attribute = master)
     #         ).update(count = current_count + 1)
-
-    if request.method == "POST":
-        form = AttributeRelationForm(request.POST, instance=unmatched_source)
-        if form.is_valid():
-            form.save()
-            # Optionally mark the source trait as matched
-            unmatched_source.matched = True
-            unmatched_source.save()
-            return HttpResponseRedirect('/matchtools/tm/')  # Redirect to get the next unmatched source trait
-        else:
-            form = AttributeRelationForm(instance=unmatched_source)
-
-    return render(request, 'matchtool/trait_match.html', {'form': form, 'unmatched_source': unmatched_source})
