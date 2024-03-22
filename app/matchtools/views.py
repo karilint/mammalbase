1d883924f7d63832c5e984b994c7848029ac4196
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2024-03-22 11:53:36 +0200

    Add previous button and location count

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
-from django.core.paginator import Paginator
 from mb.models import SourceLocation, LocationRelation
 from .location_api import LocationAPI
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -17,8 +16,7 @@ def trait_match(request):
 def source_location_list(request):
     """List all source locations to be matched with master locations"""
     filter = SourceLocationFilter(request.GET, queryset=SourceLocation.objects.is_active().select_related())
-    values=filter.qs
-    id_list = [x.id for x in values]
+    id_list = [x.id for x in filter.qs]
     paginator = Paginator(filter.qs, 10)
     page_number = request.GET.get('page')
     try:
@@ -44,9 +42,19 @@ def location_match_detail(request, id):
     result_count = result["totalResultsCount"]
     id_list = request.session.get('id_list')
     index = id_list.index(id)+1
+    previous=id_list.index(id)-1
     next_id = id_list[index] if index < len(id_list) else None
+    previous_id = id_list[previous] if previous >= 0 else None
     
-    return render(request, 'matchtool/location_match_detail.html', {'sourceLocation': sourceLocation, 'result_locations': result_locations, 'result_count': result_count, 'next_id': next_id})
+    return render(request, 'matchtool/location_match_detail.html', {
+        'sourceLocation': sourceLocation, 
+        'result_locations': result_locations, 
+        'result_count': result_count, 
+        'next_id': next_id, 
+        'current_index': index, 
+        'id_count': len(id_list), 
+        'previous_id': previous_id
+    })
 
 def match_location(request):
     geoNamesLocation = request.GET.get('geoNamesLocation')
