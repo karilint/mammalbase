2b1cd4d90c583a167b313cde2a92a307a60398bf
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-03-27 11:29:08 +0200

    remove unnecessary code

--- app/urls/matchtools.py
+++ app/urls/matchtools.py
@@ -6,11 +6,9 @@ from django.urls import path
 
 from matchtools.views import (
     trait_match_list, match_operation_endpoint
-    # trait_match
 )
 
 urlpatterns = [
     path('tm/', trait_match_list, name="info-trait-match"),
     path('match_operation/', match_operation_endpoint, name='match_operation_endpoint')
-    # path('tm/<int:source_attribute_id>', trait_match, name="trait-match")
 ]
