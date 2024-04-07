f5ff720c2c93d2cbf99eef261eb60953e1a34afb
helinal <helina.lahteenmaki@gmail.com>
2024-04-07 14:55:42 +0300

    delete unused import

--- app/matchtools/views.py
+++ app/matchtools/views.py
@@ -1,5 +1,3 @@
-from pathlib import WindowsPath
-
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
