88494953eacb3238178d3f567fbdb1c8b3801067
helinal <helina.lahteenmaki@gmail.com>
2024-04-11 13:33:01 +0300

    minor ui text fix

--- app/matchtools/filters.py
+++ app/matchtools/filters.py
@@ -1,17 +1,21 @@
 import django_filters
-from mb.models import SourceAttribute, MasterAttribute, SourceReference
+from mb.models import SourceAttribute, MasterAttribute
 
 class SourceAttributeFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='icontains', label='Attribute contains')
+    name = django_filters.CharFilter(
+        lookup_expr='icontains',
+        label='Attribute contains')
+
     reference__citation = django_filters.CharFilter(
         field_name='reference__citation',
-        label='Reference',
+        label='Reference contains',
         lookup_expr='icontains',
         method='filter_reference'
     )
+
     master_attribute = django_filters.ModelChoiceFilter(
         queryset=MasterAttribute.objects.filter(name='- Checked, Unlinked -'),
-        label='Master Attribute',
+        label='Standard Trait',
         empty_label='None')
     
     def filter_reference(self, queryset, name, value):
