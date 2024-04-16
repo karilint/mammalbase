dc593193e94094993648281c68e82a061c6ff430
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2024-04-16 19:09:12 +0300

    More refactoring, formatting and linting

--- app/imports/importers/diet_importer.py
+++ app/imports/importers/diet_importer.py
@@ -1,31 +1,41 @@
+"""Imports diet data
+"""
 from django.db import transaction
 from mb.models import DietSet, DietSetItem
 from .base_importer import BaseImporter
 
+
 class DietImporter(BaseImporter):
-    
+    """Class for diet importer
+    """
     @transaction.atomic
     def importRow(self, row):
         headers = list(row.columns.values)
-        
+
         # Common assignments
         author = self.get_author(getattr(row, 'author'))
-        reference = self.get_or_create_source_reference(getattr(row, 'references'), author)
-        entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
-        taxon = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
-        
+        reference = self.get_or_create_source_reference(
+            getattr(row, 'references'), author)
+        entityclass = self.get_or_create_entity_class(
+            getattr(row, 'taxonRank'), author)
+        taxon = self.get_or_create_source_entity(
+            getattr(row, 'verbatimScientificName'), reference, entityclass, author)
+
         column_functions = {
             'sex': self.get_choicevalue,
             'individualCount': self.possible_nan_to_zero,
             'associatedReferences': self.possible_nan_to_none,
             'samplingEffort': lambda val: self.get_or_create_time_period(val, reference, author),
-            'measurementMethod': lambda val: self.get_or_create_source_method(val, reference, author),
+            'measurementMethod': lambda val: self.get_or_create_source_method(
+                val, reference, author),
             'verbatimEventDate': self.possible_nan_to_none,
-            'verbatimLocality': lambda val: self.get_or_create_source_location(val, reference, author),
+            'verbatimLocality': lambda val: self.get_or_create_source_location(
+                val, reference, author),
         }
-        
+
         # Dictionary comprehension for conditional assignments
-        row_data = {key: func(getattr(row, key)) for key, func in column_functions.items() if key in headers}
+        row_data = {key: func(getattr(row, key)) for key,
+                    func in column_functions.items() if key in headers}
 
         # Ensure default values for keys not in headers
         default_values = {'location': None,
@@ -36,7 +46,7 @@ class DietImporter(BaseImporter):
                           'method': None,
                           'study_time': None
                           }
-        
+
         model = {
             'gender': row_data['sex'],
             'sample_size': row_data['individualCount'],
@@ -48,10 +58,9 @@ class DietImporter(BaseImporter):
             'reference': reference,
             'taxon': taxon,
         }
-        
+
         row_data = {**default_values, **model}
-        
-        
+
         # Creating or retrieving DietSet object
         diet_set,  = DietSet.objects.filter(
             **row_data)
@@ -59,37 +68,44 @@ class DietImporter(BaseImporter):
         if diet_set.exists():
             print("Existing DietSet found")
             return False
-        else:
-            diet_set = DietSet({
-                'created_by': author,
-                **row_data
-            })
-            diet_set.save()
-            print("New DietSet created:",
-                  diet_set)
-            # Create DietSet item
-            self.create_diet_set_item(row, diet_set)
-            return True
+        diet_set = DietSet({
+            'created_by': author,
+            **row_data
+        })
+        diet_set.save()
+        print("New DietSet created:",
+                diet_set)
+        # Create DietSet item
+        self.create_diet_set_item(row, diet_set)
+        return True
 
-        
-        
     def create_diet_set_item(self, row, diet_set: DietSet):
         """
         Creates a DietSetItem object from a row of data and a DietSet object.
         """
         headers = list(row.columns.values)
         if 'PartOfOrganism' in headers:
-            food_item = self.get_food_item(getattr(row, 'verbatimAssociatedTaxa'), possible_nan_to_none(getattr(row, 'PartOfOrganism')))
+            food_item = self.get_food_item(getattr(
+                row,'verbatimAssociatedTaxa'), self.possible_nan_to_none(
+                    getattr(row, 'PartOfOrganism'))
+            )
         else:
-            food_item = self.get_food_item(getattr(row, 'verbatimAssociatedTaxa'), None)
+            food_item = self.get_food_item(
+                getattr(row, 'verbatimAssociatedTaxa'), None)
         list_order = getattr(row, 'sequence')
         if 'measurementValue' in headers:
-            percentage = round(possible_nan_to_zero(getattr(row, 'measurementValue')), 3)
+            percentage = round(self.possible_nan_to_zero(
+                getattr(row, 'measurementValue')), 3)
         else:
             percentage = 0
-        old_ds = DietSetItem.objects.filter(diet_set=diet_set, food_item=food_item, list_order=list_order)
+        old_ds = DietSetItem.objects.filter(
+            diet_set=diet_set, food_item=food_item, list_order=list_order)
 
         if len(old_ds) == 0:
-            dietsetitem = DietSetItem(diet_set=diet_set, food_item=food_item, list_order=list_order, percentage=percentage) 
+            dietsetitem = DietSetItem(
+                diet_set=diet_set,
+                food_item=food_item,
+                list_order=list_order,
+                percentage=percentage
+            )
             dietsetitem.save()
-
--- app/imports/importers/occurrence_importer.py
+++ app/imports/importers/occurrence_importer.py
@@ -1,25 +1,16 @@
-from imports.importers.base_importer import BaseImporter
+"""Imports occurences
+"""
 from django.db import transaction
-from django.contrib.auth.models import User
-
 from mb.models import (
-    SourceAttribute,
-    SourceReference,
-    SourceEntity,
-    SourceMethod,
-    SourceUnit,
     ChoiceValue,
-    SourceStatistic,
-    SourceChoiceSetOption,
-    SourceChoiceSetOptionValue,
-    SourceMeasurementValue,
     Occurrence,
     Event,
     SourceHabitat)
 from .base_importer import BaseImporter
 
 class OccurrencesImporter(BaseImporter):
-    
+    """Class for occurence importer
+    """
     @transaction.atomic
     def importRow(self, row):
         """Put data of row to database.
@@ -35,36 +26,56 @@ class OccurrencesImporter(BaseImporter):
         author = self.get_author(getattr(row, 'author'))
         reference = self.get_or_create_source_reference(getattr(row, 'references'), author)
         entityclass = self.get_or_create_entity_class(getattr(row, 'taxonRank'), author)
-        verbatimScientificname = self.get_or_create_source_entity(getattr(row, 'verbatimScientificName'), reference, entityclass, author)
-        habitat, created = SourceHabitat.objects.get_or_create(habitat_type=getattr(row, 'habitatType'), habitat_percentage=getattr(row, 'habitatPercentage'), source_reference=reference, created_by=author)
+        verbatim_scientific_name = self.get_or_create_source_entity(
+            getattr(row, 'verbatimScientificName'),
+            reference, entityclass, author
+        )
+        habitat, created = SourceHabitat.objects.get_or_create(
+            habitat_type=getattr(row, 'habitatType'),
+            habitat_percentage=getattr(row, 'habitatPercentage'),
+            source_reference=reference, created_by=author
+        )
 
         created = None
-        
+
         # Create source location model
-        new_source_location = self.get_or_create_source_location(getattr(row, 'verbatimLocality'), reference, author)
-        new_event, created = Event.objects.get_or_create(verbatim_event_date=getattr(row, 'verbatimEventDate'), source_habitat=habitat)
+        new_source_location = self.get_or_create_source_location(
+            getattr(row, 'verbatimLocality'),
+            reference, author
+        )
+        new_event, created = Event.objects.get_or_create(
+            verbatim_event_date=getattr(row, 'verbatimEventDate'),
+            source_habitat=habitat
+        )
 
         gender = str(getattr(row, 'sex'))
 
         life_stage = str(getattr(row, 'lifeStage'))
 
-        if gender == "nan" or gender == "":
+        if gender in ["nan", ""]:
             gender = None
         else:
+            gender, created = ChoiceValue.objects.get_or_create(
+                choice_set="Gender", caption=gender.capitalize()
+            )
 
-            gender, created = ChoiceValue.objects.get_or_create(choice_set="Gender", caption=gender.capitalize())
-
-        if life_stage == "nan" or life_stage == "":
+        if life_stage in ["nan", ""]:
             life_stage = None
         else:
-            life_stage, created = ChoiceValue.objects.get_or_create(choice_set="Lifestage", caption=life_stage.capitalize())
-        
-        obj, created = Occurrence.objects.get_or_create(source_reference=reference, event=new_event, source_location=new_source_location, source_entity=verbatimScientificname,
-                                                    organism_quantity=getattr(row, 'organismQuantity'), organism_quantity_type=getattr(row, 'organismQuantityType'), gender=gender, 
-                                                    life_stage=life_stage,
-                                                    occurrence_remarks=getattr(row, 'occurrenceRemarks'), associated_references=getattr(row, 'associatedReferences'))
-        if created:
-            return True
-        else:
-            return False
+            life_stage, created = ChoiceValue.objects.get_or_create(
+                choice_set="Lifestage", caption=life_stage.capitalize()
+            )
 
+        created = Occurrence.objects.get_or_create(
+            source_reference=reference,
+            event=new_event,
+            source_location=new_source_location,
+            source_entity=verbatim_scientific_name,
+            organism_quantity=getattr(row, 'organismQuantity'),
+            organism_quantity_type=getattr(row, 'organismQuantityType'),
+            gender=gender,
+            life_stage=life_stage,
+            occurrence_remarks=getattr(row, 'occurrenceRemarks'),
+            associated_references=getattr(row, 'associatedReferences')
+        )
+        return bool(created)
