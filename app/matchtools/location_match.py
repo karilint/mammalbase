43cca22b1ef0573bc14a52172d4656f3bad2ce46
Patrick Salmi <patrick.salmi@helsinki.fi>
2024-04-17 13:56:53 +0300

    fix location-match hierarchy list + refactoring

--- app/matchtools/location_match.py
+++ app/matchtools/location_match.py
@@ -1,6 +1,5 @@
 from mb.models import SourceLocation, MasterLocation, LocationRelation
 from matchtools.location_api import LocationAPI
-from django.db.models import Q
 
 def create_master_location(geoNamesLocation: dict, prev_location: MasterLocation):
     name = geoNamesLocation["name"]
@@ -42,7 +41,7 @@ def add_locations(geo_names_location, source_location_id):
     source_location = SourceLocation.objects.get(id=source_location_id)
     api = LocationAPI()
     hierarchy_list = api.get_location_hierarchy(geo_names_location["geonameId"])["geonames"]
-    hierarchy_list = [location for location in hierarchy_list if location["geonameId"] != geo_names_location["geonameId"]]
+    hierarchy_list = [location for location in hierarchy_list if location["name"] != geo_names_location["name"]]
     
     locations = hierarchy_list + [geo_names_location]
     continent = next((location["name"] for location in locations if location["fcode"] == "CONT"), None)
@@ -53,10 +52,12 @@ def add_locations(geo_names_location, source_location_id):
                 location["continent"] = continent
     
     added_locations = []
-    for i, location in enumerate(locations):
-        prev_location_id = added_locations[i-1] if i > 0 else None
-        added_location = create_master_location(location, prev_location_id)
-        added_locations.append(added_location)
+    new_location = (None, None)
+    for location in locations:
+        prev_location = new_location[0] if new_location[0] else None
+        new_location = create_master_location(location, prev_location)
+        if new_location[1]:
+            added_locations.append(new_location[0])
         
     if added_locations[-1] is not None:
         match_locations(added_locations[-1], source_location)
