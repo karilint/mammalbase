f6174553581a4ae1365a5a3ed4a1d6a119e838f8
Sanni Tuomisto <sanni.tuomisto@helsinki.fi>
2024-04-19 14:24:49 +0300

    Add taxonranks and update formatting

--- app/imports/validation_lib/diet_set_validation.py
+++ app/imports/validation_lib/diet_set_validation.py
@@ -10,24 +10,23 @@ class Diet_set_validation(Validation):
         super().__init__()
 
         self.rules = {
-            "author":                       "required|author", 
-            "verbatimScientificName":       "required|alpha", 
-            "taxonRank":                    "in:Subspecies,Varietas,Forma,Species,Genus,Nothogenus,Nothospecies,Nothosubspecies,Family,nan", 
-            "verbatimLocality":             "max:250", 
-            "habitat" :                     "max:250", 
-            "samplingEffort" :              "max:250",
-            "sex":                          "choiceValue:gender", 
-            "individualCount" :             "digits",
-            "verbatimEventDate":            "max:250", 
-            "verbatimAssociatedTaxa" :      "max:250", 
-            #"PartOfOrganism" :              "in:BARK,BLOOD,BONES,BUD CARRION,EGGS,EXUDATES,FECES,FLOWER,FRUIT,LARVAE,LEAF,MINERAL,NECTAR/JUICE,NONE,POLLEN,ROOT,SEED,SHOOT,STEM,UNKNOWN,WHOLE",
-            "sequence" :                    "max:250", 
-            "measurementValue" :            "digis|min:0", 
-            "associatedReferences" :        "max:250", 
-            "references":                   "required|min:10|max:500|regex:.*([1-2][0-9]{3})", 
+            "author":                       "required|author",
+            "verbatimScientificName":       "required|alpha|max:500",
+            "taxonRank":                    "in:Subspecies,Varietas,Forma,Species,Genus,Nothogenus,Nothospecies,Nothosubspecies,Family,nan,species,subspecies,varietas,forma,family,genus,nothogenus,nothospecies,nothosubspecies,SPECIES,SUBSPECIES,VARIETAS,FORMA,FAMILY,GENUS,NOTHOSPECIES,NOTHOSUBSPECIES,NOTHOSPECIES,NOTHOSUBSPECIES",
+            "verbatimLocality":             "max:250",
+            "habitat":                     "max:250",
+            "samplingEffort":              "max:250",
+            "sex":                          "choiceValue:gender",
+            "individualCount":             "digits",
+            "verbatimEventDate":            "max:250",
+            "verbatimAssociatedTaxa":      "max:250",
+            # "PartOfOrganism" :              "in:BARK,BLOOD,BONES,BUD CARRION,EGGS,EXUDATES,FECES,FLOWER,FRUIT,LARVAE,LEAF,MINERAL,NECTAR/JUICE,NONE,POLLEN,ROOT,SEED,SHOOT,STEM,UNKNOWN,WHOLE",
+            "sequence":                    "max:250",
+            "measurementValue":            "digis|min:0",
+            "associatedReferences":        "max:250",
+            "references":                   "required|min:10|max:500|regex:.*([1-2][0-9]{3})",
         }
 
-    
         self.data = {
             "author": "",
             "verbatimScientificName": "",
@@ -46,6 +45,3 @@ class Diet_set_validation(Validation):
             "associatedReferences": "",
             "references": ""
         }
-    
-
-
