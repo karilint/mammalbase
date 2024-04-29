2e86bcb3a0ac5a8629797795a26967b782234ed6
eerolasi <93593718+eerolasi@users.noreply.github.com>
2024-04-29 19:59:23 +0300

    unify list_displays

--- app/mb/admin.py
+++ app/mb/admin.py
@@ -59,20 +59,30 @@ admin.site.register([TaxonomicUnits, ])
 #    search_fields = ['name', ]
 #    history_list_display = ['name', ]
 
+
 class AttributeGroupRelationInline(admin.TabularInline):
     model = AttributeGroupRelation
     extra = 0
 
+
 class AttributeRelationInline(admin.TabularInline):
     model = AttributeRelation
     extra = 0
-    
+
+
 @admin.register(AttributeGroupRelation)
 class AttributeGroupRelationAdmin(SimpleHistoryAdmin):
     search_fields = ['group__name', 'attribute__name']
     list_filter = [('group', admin.RelatedOnlyFieldListFilter),
                    ('attribute', admin.RelatedOnlyFieldListFilter)]
-    list_display = ('group', 'attribute')
+    list_display = ('group', 'attribute_name')
+
+    def attribute_name(self, obj):
+        return obj.attribute.name
+
+    attribute_name.admin_order_field = 'attribute__name'
+    attribute_name.short_description = 'Attribute'
+
 
 @admin.register(AttributeRelation)
 class AttributeRelationAdmin(SimpleHistoryAdmin):
@@ -81,7 +91,27 @@ class AttributeRelationAdmin(SimpleHistoryAdmin):
         'master_attribute__name',
         'master_attribute__reference__citation']
     list_filter = [('master_attribute', admin.RelatedOnlyFieldListFilter)]
-    list_display = ('source_attribute', 'master_attribute')
+    list_display = ('source_attribute_name',
+                    'master_attribute_name', 'master_attribute_reference')
+
+    def source_attribute_name(self, obj):
+        return obj.source_attribute.name
+
+    source_attribute_name.admin_order_field = 'source_attribute__name'
+    source_attribute_name.short_description = 'Source Attribute'
+
+    def master_attribute_name(self, obj):
+        return obj.master_attribute.name
+
+    master_attribute_name.admin_order_field = 'master_attribute__name'
+    master_attribute_name.short_description = 'Master Attribute'
+
+    def master_attribute_reference(self, obj):
+        return obj.master_attribute.reference.citation
+
+    master_attribute_reference.short_description = 'Master Attribute Reference'
+    master_attribute_reference.admin_order_field = 'master_attribute__reference__citation'
+
 
 @admin.register(ChoiceSetOptionRelation)
 class ChoiceSetOptionRelationAdmin(SimpleHistoryAdmin):
@@ -91,7 +121,13 @@ class ChoiceSetOptionRelationAdmin(SimpleHistoryAdmin):
     list_filter = [
         ('master_choiceset_option',
          admin.RelatedOnlyFieldListFilter)]
-    list_display = ('source_choiceset_option', 'master_choiceset_option')
+    list_display = ('source_choiceset_option_name', 'master_choiceset_option')
+
+    def source_choiceset_option_name(self, obj):
+        return obj.source_choiceset_option.name
+
+    source_choiceset_option_name.admin_order_field = 'source_choiceset_option__name'
+    source_choiceset_option_name.short_description = 'Source Choice Set Option'
 
 
 @admin.register(ChoiceValue)
@@ -104,14 +140,32 @@ class ChoiceValueAdmin(SimpleHistoryAdmin):
 @admin.register(DietSetItem)
 class DietSetItemAdmin(SimpleHistoryAdmin):
     search_fields = ['diet_set__reference__citation', 'food_item__name']
-    list_display = ('food_item', 'diet_set')
+    list_display = ('food_item_name', 'diet_set_reference')
+
+    def food_item_name(self, obj):
+        return obj.food_item.name
+
+    food_item_name.short_description = 'Food Item'
+    food_item_name.admin_order_field = 'food_item__name'
+
+    def diet_set_reference(self, obj):
+        return obj.diet_set.reference.citation
+
+    diet_set_reference.short_description = 'Reference'
+    diet_set_reference.admin_order_field = 'diet_set__reference__citation'
 
 
 @admin.register(DietSet)
 class DietSetAdmin(SimpleHistoryAdmin):
     search_fields = ['taxon__name', 'reference__citation']
-    list_display = ('taxon', 'reference', 'data_quality_score')
     list_filter = ['data_quality_score']
+    list_display = ('taxon', 'reference_name')
+
+    def reference_name(self, obj):
+        return obj.reference.citation
+
+    reference_name.short_description = 'Reference'
+    reference_name.admin_order_field = 'reference__citation'
 
 
 @admin.register(EntityClass)
@@ -127,9 +181,15 @@ class EntityRelationAdmin(SimpleHistoryAdmin):
         'master_entity__name',
         'master_entity__reference__citation']
     list_display = ('source_entity',
-                    'master_entity')
+                    'master_entity', 'master_entity_reference')
     list_filter = [('relation_status', admin.RelatedOnlyFieldListFilter),
                    ('data_status', admin.RelatedOnlyFieldListFilter)]
+
+    def master_entity_reference(self, obj):
+        return obj.master_entity.reference.citation
+
+    master_entity_reference.short_description = 'Master Entity Reference'
+    master_entity_reference.admin_order_field = 'master_entity__reference__citation'
 
 
 @admin.register(FoodItem)
@@ -137,7 +197,18 @@ class FoodItemAdmin(SimpleHistoryAdmin):
     search_fields = ['name']
     list_filter = [('part', admin.RelatedOnlyFieldListFilter),
                    ('is_cultivar', admin.BooleanFieldListFilter)]
-    list_display = ('name', 'part')
+    list_display = ('food_item_name', 'food_item_part')
+
+    def food_item_name(self, obj):
+        return obj.name
+
+    food_item_name.admin_order_field = 'name'
+    food_item_name.short_description = 'Food Item'
+
+    def food_item_part(self, obj):
+        return obj.part.caption
+
+    food_item_part.admin_order_field = 'part__caption'
 
 
 @admin.register(MasterAttribute)
@@ -145,8 +216,9 @@ class MasterAttributeAdmin(SimpleHistoryAdmin):
     search_fields = ['entity__name', 'name', 'unit__print_name']
     list_filter = [('entity', admin.RelatedOnlyFieldListFilter),
                    'name', ('unit', admin.RelatedOnlyFieldListFilter)]
-    list_display = ('name', 'entity', 'unit')
+    list_display = ('name', 'unit')
     inlines = [AttributeGroupRelationInline]
+
 
 
 @admin.register(MasterAttributeGroup)
@@ -187,13 +259,31 @@ class MasterReferenceAdmin(SimpleHistoryAdmin):
 @admin.register(ProximateAnalysisItem)
 class ProximateAnalysisItemAdmin(SimpleHistoryAdmin):
     search_fields = ['proximate_analysis__reference__citation', 'forage__name']
-    list_display = ('forage', 'proximate_analysis')
+    list_display = ('forage_name', 'proximate_analysis_reference')
+
+    def forage_name(self, obj):
+        return obj.forage.name
+
+    forage_name.short_description = 'Forage'
+    forage_name.admin_order_field = 'forage__name'
+
+    def proximate_analysis_reference(self, obj):
+        return obj.proximate_analysis.reference.citation
+
+    proximate_analysis_reference.short_description = 'Reference'
+    proximate_analysis_reference.admin_order_field = 'proximate_analysis__reference__citation'
 
 
 @admin.register(ProximateAnalysis)
 class ProximateAnalysisAdmin(SimpleHistoryAdmin):
     search_fields = ['reference__citation']
-    list_display = ('reference',)
+    list_display = ('reference_citation',)
+
+    def reference_citation(self, obj):
+        return obj.reference.citation
+
+    reference_citation.short_description = 'Reference'
+    reference_citation.admin_order_field = 'reference__citation'
 
 
 @admin.register(SourceAttribute)
@@ -201,41 +291,75 @@ class SourceAttributeAdmin(SimpleHistoryAdmin):
     search_fields = ['entity__name', 'name']
     list_filter = [
         ('master_attribute',
-         admin.RelatedOnlyFieldListFilter),
+         admin.RelatedOnlyFieldListFilter), 
         'type']
-    list_display = ('name', 'type')
+    list_display = ('name', )
     inlines = [AttributeRelationInline]
+
 
 @admin.register(SourceChoiceSetOptionValue)
 class SourceChoiceSetOptionValueAdmin(SimpleHistoryAdmin):
     search_fields = [
         'source_choiceset_option__source_attribute__name',
         'source_choiceset_option__name']
-    list_display = ('source_entity', 'source_choiceset_option')
+    list_display = ('source_choiceset_option_source_attribute_name',
+                    'source_choiceset_option_name')
+
+    def source_choiceset_option_source_attribute_name(self, obj):
+        return obj.source_choiceset_option.source_attribute.name
+
+    source_choiceset_option_source_attribute_name.short_description = 'Source Attribute'
+    source_choiceset_option_source_attribute_name.admin_order_field = \
+        'source_choiceset_option__source_attribute__name'
+
+    def source_choiceset_option_name(self, obj):
+        return obj.source_choiceset_option.name
+
+    source_choiceset_option_name.short_description = 'Source Choice Set Option'
+    source_choiceset_option_name.admin_order_field = 'source_choiceset_option__name'
 
 
 @admin.register(SourceChoiceSetOption)
 class SourceChoiceSetOptionAdmin(SimpleHistoryAdmin):
     search_fields = ['source_attribute__name', 'name']
-    list_display = ('source_attribute', 'name')
+    list_display = ('source_attribute_name', 'source_choice_set_option')
+
+    def source_attribute_name(self, obj):
+        return obj.source_attribute.name
+
+    source_attribute_name.short_description = 'Source Attribute'
+    source_attribute_name.admin_order_field = 'source_attribute__name'
+
+    def source_choice_set_option(self, obj):
+        return obj.name
+
+    source_choice_set_option.short_description = 'Source Choice Set Option'
+    source_choice_set_option.admin_order_field = 'name'
 
 
 @admin.register(SourceEntity)
 class SourceEntityAdmin(SimpleHistoryAdmin):
     search_fields = ['name']
-    list_display = ('name', 'entity')
+    list_display = ('name',)
     list_filter = [('entity', admin.RelatedOnlyFieldListFilter)]
 
 
 @admin.register(SourceLocation)
 class SourceLocationAdmin(SimpleHistoryAdmin):
     search_fields = ['name']
+    list_display = ('name',)
 
 
 @admin.register(SourceMeasurementValue)
 class SourceMeasurementValueAdmin(SimpleHistoryAdmin):
     search_fields = ['source_attribute__name', 'n_total']
-    list_display = ('source_attribute', 'n_total')
+    list_display = ('source_attribute_name', 'n_total')
+
+    def source_attribute_name(self, obj):
+        return obj.source_attribute.name
+
+    source_attribute_name.short_description = 'Source Attribute'
+    source_attribute_name.admin_order_field = 'source_attribute__name'
 
 
 @admin.register(SourceMethod)
@@ -248,7 +372,7 @@ class SourceMethodAdmin(SimpleHistoryAdmin):
 class SourceReferenceAdmin(SimpleHistoryAdmin):
     search_fields = ['citation']
     list_filter = ['status']
-    list_display = ('citation', 'status')
+    list_display = ('citation',)
 
 
 @admin.register(SourceStatistic)
@@ -265,7 +389,7 @@ class SourceUnitAdmin(SimpleHistoryAdmin):
 class TimePeriodAdmin(SimpleHistoryAdmin):
     search_fields = ['name']
     list_filter = ['time_in_months']
-    list_display = ('name', 'time_in_months')
+    list_display = ('name', )
 
 
 admin.site.register(MasterUnit, SimpleHistoryAdmin)
